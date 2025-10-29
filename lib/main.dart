24f0c5a134b802f70b58b240f3b01e701d42e62c
tazkiaalyani <tzkiaalyani@gmail.com>
2025-10-29 16:15:19 +0700

    Tutorial 6 selesai

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:football_news/menu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Football News',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
+            .copyWith(secondary: Colors.blueAccent[400]),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
